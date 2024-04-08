45803c7c5989153accc061a2ac4ede0c9afdeafc
George <georgiyozhegov.rs@gmail.com>
2024-04-08 19:56:12 +0400

    treesitter parsers for css, html csharp

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-      ensure_installed = {"rust", "python", "c", "json", "lua", "nasm", "html", "css"},
+      ensure_installed = {"rust", "python", "c", "json", "lua", "nasm", "html", "css", "c_sharp"},
       highlight = {
             enable = true,
       }
