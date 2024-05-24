8f19d97cf2bc9faf5ac1024c59cbda78eddc40e6
George <georgiyozhegov.rs@gmail.com>
2024-05-24 12:35:24 +0400

    Remove: nvim :PlugInstall in setup script

--- setup.sh
+++ setup.sh
@@ -13,5 +13,3 @@ if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
-
-#nvim -c "PlugInstall<\CR>" -c "qa"
