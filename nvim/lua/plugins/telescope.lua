b1d628925a75dd9539cdab53f3c573cf347eadf8
George <georgiyozhegov.rs@gmail.com>
2024-07-22 21:45:24 +0400

    Remove: live-grep and command-search in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,16 +3,7 @@ require("telescope").setup({
             find_files = {
                   theme = "dropdown"
             },
-            commands = {
-                  theme = "dropdown"
-            },
-            live_grep = {
-                  theme = "dropdown"
-            }
       }
 })
 
 vim.keymap.set("n", "f", ":Telescope find_files<cr>")
-vim.keymap.set("n", "c", ":Telescope commands<cr>")
-vim.keymap.set("n", "s", ":Telescope live_grep<cr>")
-
